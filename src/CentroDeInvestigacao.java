de0280d18f38129736ff13ab3539632b10ddc184
ventura2709 <ventura.tfp@gmail.com>
2019-11-26 15:29:28 +0000

    Adicionadas todas as classes

---
+++ src/CentroDeInvestigacao.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class CentroDeInvestigacao {
+    public static void novoProjeto(ArrayList<Projeto> ???){
+
+    }
+    public static void ListarProjetosNConcluidosATempo(ArrayList<Projeto> ???){
+
+    }
+    public static void ListarProjetosConcluidos(ArrayList<Projeto> ???){
+
+    }
+}
