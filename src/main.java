e657b444e54d6bafc51e6943547816f6dade9a70
DreSilva <2000.andre.silva@gmail.com>
2019-11-28 19:49:05 +0000

    Minor Change

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        ArrayList<CentroDeInvestigacao> centros = new ArrayList<CentroDeInvestigacao>(24);
+        CentroDeInvestigacao CISUC = new CentroDeInvestigacao();
     }
 }
