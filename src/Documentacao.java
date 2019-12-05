304fe592786c5172d84d6fca24f4907b8e7b31ec
ventura2709 <ventura.tfp@gmail.com>
2019-12-05 19:20:04 +0000

    main para Main

--- src/Documentacao.java
+++ src/Documentacao.java
@@ -7,5 +7,4 @@ public class Documentacao extends Tarefa {
     public Documentacao(int diaIn, int mesIn, int anoIn, int duracao, int percentagemConc){
         super(diaIn, mesIn, anoIn, duracao, percentagemConc);
     }
-
 }
