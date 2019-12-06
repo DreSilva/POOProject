d7e38d963772f2f6cdb4ac26753c4668e97aac38
ventura2709 <ventura.tfp@gmail.com>
2019-12-06 12:50:14 +0000

    adicionados butoes

--- src/CentroDeInvestigacao.java
+++ src/CentroDeInvestigacao.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
-public class CentroDeInvestigacao {
+public class CentroDeInvestigacao{
     protected String nome;
+    protected ArrayList<Projeto> projetos = new ArrayList<Projeto>();
 
     public CentroDeInvestigacao(String nome){
         this.nome=nome;
@@ -11,14 +12,14 @@ public class CentroDeInvestigacao {
         return nome;
     }
 
-    public static void main(String[] args) {
-        ArrayList<Projeto> projetos = new ArrayList<Projeto>(24);
-    }
-
-    public static void novoProjeto(String nome, String acronimo, int diaIn, int mesIn, int anoIn, int duracao){
+    /*public static void novoProjeto(String nome, String acronimo, int diaIn, int mesIn, int anoIn, int duracao){
         Projeto p1 = new Projeto("Projeto POO", "PPOO", 28,11,2019,1);
         Projeto p2 = new Projeto("Projeto IRC", "PIRC", 25,11,2019,2);
         Projeto p3 = new Projeto("Projeto TI", "PTI", 23,11,2019,3);
+    }*/
+
+    public static void listarProjetos(){
+
     }
 
     public static void ListarProjetosNConcluidosATempo(ArrayList<Projeto> projetos){
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,37 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame{
+    private JLabel label;
     private JPanel panelInicial;
-    private JFrame frameCentros;
+    private JButton listarPessoas,listarProjetos,adicionarProjeto,listarProjetosNaoConcluidosNaDataEstimada,listarProjetosConcluidos;
 
     public Main(String nomeDoCentro){
         super();
+
         this.panelInicial = new JPanel();
         panelInicial.setLayout(new GridLayout(3,2));
-        JButton listarPessoas = new JButton("Listar Pessoas");
-        JButton listarProjetos = new JButton("Listar Projetos");
-        JButton adicionarProjeto = new JButton("Adicionar Projeto");
-        JButton listarProjetosNaoConcluidosNaDataEstimada = new JButton("Listar Projetos não concluidos na data estimada");
-        JButton listarProjetosConcluidos = new JButton("Listar Projetos concluidos");
-        JLabel label = new JLabel(nomeDoCentro);
+
+        listarPessoas = new JButton("Listar Pessoas");
+        listarProjetos = new JButton("Listar Projetos");
+        adicionarProjeto = new JButton("Adicionar Projeto");
+        listarProjetosNaoConcluidosNaDataEstimada = new JButton("Listar Projetos não concluidos na data estimada");
+        listarProjetosConcluidos = new JButton("Listar Projetos concluidos");
+        label = new JLabel(nomeDoCentro);
+
+        listarPessoas.addActionListener(new ButtonListener());
+        listarPessoas.setActionCommand("Listar Pessoas");
+
+        listarProjetos.addActionListener(new ButtonListener());
+        listarProjetos.setActionCommand("Listar Projetos");
+
+        adicionarProjeto.addActionListener(new ButtonListener());
+        adicionarProjeto.setActionCommand("Adicionar Projeto");
+
+        listarProjetosNaoConcluidosNaDataEstimada.addActionListener(new ButtonListener());;
+        listarProjetosNaoConcluidosNaDataEstimada.setActionCommand("Listar PNCNDE");
+
+        listarProjetosConcluidos.addActionListener(new ButtonListener());
+        listarProjetosConcluidos.setActionCommand("Listar Projetos Concluidos");
 
         panelInicial.add(label);
         panelInicial.add(listarPessoas);
@@ -24,16 +42,34 @@ public class Main extends JFrame{
         panelInicial.add(adicionarProjeto);
         panelInicial.add(listarProjetosNaoConcluidosNaDataEstimada);
         panelInicial.add(listarProjetosConcluidos);
-        this.add(panelInicial);
 
+        this.add(panelInicial);
     }
-    public void main(String[] args) {
+
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            String cmd = e.getActionCommand();
+            if (cmd.equals("Listar Pessoas"))
+                System.out.println("a");
+            else if (cmd.equals("Listar Projetos"))
+                System.out.println("b");
+            else if (cmd.equals("Adicionar Projeto"))
+                System.out.println("c");
+            else if (cmd.equals("Listar PNCNDE"))
+                System.out.println("d");
+            else if (cmd.equals("Listar Projetos Concluidos"))
+                System.out.println("e");
+        }
+    }
+
+    public static void main(String[] args) {
         CentroDeInvestigacao CISUC = new CentroDeInvestigacao("CISUC");
-        this.frameCentros = new JFrame();
-        this.frameCentros.setTitle("Centro de Investigação");
-        this.frameCentros.setSize(400,150);
-        this.frameCentros.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frameCentros.setVisible(true);
+        Main frameCentros = new Main(CISUC.getNome());
+        frameCentros.setTitle("Centro de Investigação");
+        frameCentros.setSize(700,150);
+        frameCentros.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameCentros.setVisible(true);
     }
 }
 
